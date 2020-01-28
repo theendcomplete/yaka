96bddb3f08d0f64880d4dfde15e32cf86e02a925
Nikkie Grom <nickonizator@gmail.com>
2020-01-28 10:56:14 +0300

    format refund class

--- lib/yaka/refund.rb
+++ lib/yaka/refund.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
 module Yaka
+  # This class is used for sending refunds to Yandex Kassa.
+  # https://kassa.yandex.ru/developers/payments/refunds#%D0%B2%D0%BE%D0%B7%D0%B2%D1%80%D0%B0%D1%82%D1%8B
   class Refund < BasicStruct
-    # This class is used for sending refunds to Yandex Kassa.
-    # https://kassa.yandex.ru/developers/payments/refunds#%D0%B2%D0%BE%D0%B7%D0%B2%D1%80%D0%B0%D1%82%D1%8B
-
-        attribute :amount, BasicStruct do
-          attribute :value, Types::String
-          attribute :currency, Types::String
-        end
-      attribute :payment_id, Types::String
+    attribute :amount, BasicStruct do
+      attribute :value, Types::String
+      attribute :currency, Types::String
     end
+    attribute :payment_id, Types::String
   end
 end
